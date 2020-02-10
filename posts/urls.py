b0f73686468960bf6a84a85e613bf9a3681ca314
singertwist <regis.terpend@yahoo.fr>
2020-02-10 11:00:10 +0100

    Modification design + mise à jour pattern url

--- posts/urls.py
+++ posts/urls.py
@@ -1,27 +1,14 @@
 from django.conf.urls import url
-
-# from .views import (
-# 		home,
-# 		article,
-# 		contact,
-# )
-
-# urlpatterns = [
-#     url(r'accueil/$', home),
-#     url(r'article/(?P<slug>.+)-(?P<id>\d+)$', article),
-#     url(r'contact/$', contact),
-#     #url(r'^posts/$', <nom de l'application, ici posts>.view.<nom de la fonction dans la vue>,
-# ]
-
+from django.urls import path
 from posts import views
 
 urlpatterns = [
-    url(r'^/?$', views.home, name='home'),
-    url(r'article/(?P<slug>.+)-(?P<id>\d+)$',  views.article, name='article'),
-    url(r'portfolio/(?P<slug>.+)-(?P<article>\d+)$',  views.portfolio, name='portfolio'),
-    url(r'carte/(?P<slug>.+)-(?P<article>\d+)$',  views.map, name='map'),
-    url(r'contact/$', views.contact, name='contact'),
-    url(r'categories/(?P<slug>.+)-(?P<id>\d+)$', views.souscategorie_display, name='souscategorie_display'),
-    url(r'categories/$', views.categorie_display, name='categorie_display'),
-    url(r'a-propos/$', views.apropos, name='apropos'),
+    path(r'', views.home, name='home'),
+    path(r'article/<str:slug>-<int:id>',  views.article, name='article'),
+    path(r'portfolio/<str:slug>-<int:article>',  views.portfolio, name='portfolio'),
+    path(r'carte/<str:slug>-<int:article>',  views.map, name='map'),
+    path(r'contact/', views.contact, name='contact'),
+    path(r'categories/<str:slug>-<int:id>', views.souscategorie_display, name='souscategorie_display'),
+    path(r'categories/', views.categorie_display, name='categorie_display'),
+    path(r'a-propos/', views.apropos, name='apropos'),
 ]
